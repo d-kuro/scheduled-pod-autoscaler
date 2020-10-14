2d13f1788e6be6aa9011a145c7affe5956d0d8d3
d-kuro <kurosawa7620@gmail.com>
2020-10-15 00:53:00 +0900

    update ScheduledPodAutoscaler api

--- apis/autoscaling/v1/scheduledpodautoscaler_types.go
+++ apis/autoscaling/v1/scheduledpodautoscaler_types.go
@@ -17,6 +17,9 @@ limitations under the License.
 package v1
 
 import (
+	"time"
+
+	autoscalingv2beta2 "k8s.io/api/autoscaling/v2beta2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,8 +31,76 @@ type ScheduledPodAutoscalerSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of ScheduledPodAutoscaler. Edit ScheduledPodAutoscaler_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// HorizontalPodAutoscalerSpec is HorizontalPodAutoscaler v2beta2 API spec.
+	// +kubebuilder:validation:Required
+	HorizontalPodAutoscalerSpec autoscalingv2beta2.HorizontalPodAutoscalerSpec `json:"horizontalPodAutoscalerSpec"`
+
+	// ScheduleList is list of schedule info.
+	// +optional
+	ScheduleList []Schedule `json:"schedule"`
+}
+
+// Schedule is schedule info.
+type Schedule struct {
+	// Suspend indicates whether to suspend this schedule.
+	// +optional
+	Suspend bool `json:"suspend,omitempty"`
+
+	// Name is schedule name.
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+
+	// Description is schedule description.
+	// +optional
+	Description string `json:"description,omitempty"`
+
+	// MinReplicas is the lower limit for the number of replicas to which the autoscaler can scale down.
+	// It defaults to 1 pod.
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Minimum=1
+	// +optional
+	MinReplicas *int32 `json:"minReplicas,omitempty"`
+
+	// MaxReplicas is the upper limit for the number of replicas to which the autoscaler can scale up.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Minimum=1
+	MaxReplicas int32 `json:"maxReplicas"`
+
+	// Metrics contains the specifications for which to use to calculate the desired replica count.
+	// +optional
+	Metrics []autoscalingv2beta2.MetricSpec `json:"metrics,omitempty"`
+
+	// Behavior configures the scaling behavior of the target in both Up and Down directions.
+	// +optional
+	// Behavior *autoscalingv2beta2.HorizontalPodAutoscalerBehavior `json:"behavior,omitempty"`
+
+	// StartDayOfWeek is scaling start day of week.
+	// +kubebuilder:validation:Enum=Monday;Tuesday;Wednesday;Thursday;Friday;Saturday;Sunday
+	// +optional
+	StartDayOfWeek string `json:"startDayOfWeek"`
+
+	// EndDayOfWeek is scaling end day of week.
+	// +kubebuilder:validation:Enum=Monday;Tuesday;Wednesday;Thursday;Friday;Saturday;Sunday
+	// +optional
+	EndDayOfWeek string `json:"endDayOfWeek"`
+
+	// StartTime is scaling start time.
+	// +kubebuiler:validation:Required
+	StartTime string `json:"startTime"`
+
+	// EndTime is scaling end time.
+	// +kubebuiler:validation:Required
+	EndTime string `json:"endTime"`
+}
+
+var Weekdays = map[string]time.Weekday{
+	"Monday":    time.Weekday(1),
+	"Tuesday":   time.Weekday(2),
+	"Wednesday": time.Weekday(3),
+	"Thursday":  time.Weekday(4),
+	"Friday":    time.Weekday(5),
+	"Saturday":  time.Weekday(6),
+	"Sunday":    time.Weekday(0),
 }
 
 // ScheduledPodAutoscalerStatus defines the observed state of ScheduledPodAutoscaler
